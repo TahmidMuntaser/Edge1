db92f927b225a9afe5eae797aba3f77647f3145f
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2024-11-21 18:14:51 +0600

    js

--- sample.js
+++ sample.js
@@ -1,6 +1,16 @@
-alert("Alert message!!")
+// alert("Alert message!!")
 
 function test(){
-    alert("This is a test function")
+   document.getElementById("test").style.color = "red";
+   document.getElementById("test").innerHTML = "Hello World!";
+   document.getElementById("test").append("Test")
+}
+
+function test2(){
+    var f = parseInt(document.getElementById("first").value);
+    var s = parseInt(document.getElementById("second").value);
+    var total = f + s;
+
+    document.getElementById("res").innerHTML = total;
 }
 
