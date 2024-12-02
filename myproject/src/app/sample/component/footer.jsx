bfce43878b3df144e3913e9e188c06ee3c1572a9
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2024-12-02 17:24:21 +0600

    bg jsx added

--- myproject/src/app/sample/component/footer.jsx
+++ myproject/src/app/sample/component/footer.jsx
@@ -1,7 +1,9 @@
+
 export function Footer(){
     return(
-        <>
-           
-        </>
+        <footer className="bg-blue-950 p-8 text-white text-center">
+            © 2024 Travel Explorer. All rights reserved.
+        </footer>
+      
     );
 }
--- myproject/src/app/sample/page.jsx
+++ myproject/src/app/sample/page.jsx
@@ -1,12 +1,14 @@
 import { Header } from "./component/header";
 import { Footer } from "./component/footer";
+import { Background } from "./component/background";
 
 export default function Home() {
     return (
         <>
             <Header/>
+            <Background/> 
             <Footer/>
-        </>
+        </> 
        
     )
 }
