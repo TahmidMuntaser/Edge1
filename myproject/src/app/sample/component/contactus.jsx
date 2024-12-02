67f481d7515f1d23b3159f6748bac39ee3b9a218
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2024-12-02 19:50:51 +0600

    contactus page updated

---
+++ myproject/src/app/sample/component/contactus.jsx
@@ -0,0 +1,12 @@
+
+export function ContactUs() {
+    return (
+        <div>
+            <h1 className="text-2xl md:text-3xl text-slate-800 font-bold text-center pt-8 pb-3">Contact Us</h1>
+            <p className="text-lg text-slate-800 text-center pt-8 pb-3">We would love to hear from you!</p>
+            <a href="mailto:info@travelexplorer.com" className="text-lg text-center block mb-4 text-blue-900">
+                <u>info@travelexplorer.com</u>
+            </a>
+        </div>
+    );
+}
--- myproject/src/app/sample/page.jsx
+++ myproject/src/app/sample/page.jsx
@@ -1,12 +1,14 @@
 import { Header } from "./component/header";
 import { Footer } from "./component/footer";
 import { Background } from "./component/background";
+import { ContactUs } from "./component/contactus";
 
 export default function Home() {
     return (
         <>
             <Header/>
-                <Background/> 
+            <Background/> 
+            <ContactUs/>
             <Footer/>
         </> 
        
