8032d723cff3478b38159fa43d1fd8267d7932a5
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2024-12-02 20:42:59 +0600

    PopularDestination page updated

--- myproject/src/app/sample/page.jsx
+++ myproject/src/app/sample/page.jsx
@@ -3,12 +3,14 @@ import { Footer } from "./component/footer";
 import { Background } from "./component/background";
 import { ContactUs } from "./component/contactus";
 import { PricingPlans } from "./component/pricingplans";
+import { PopularDestinations } from "./component/populardestinations";
 
 export default function Home() {
     return (
         <>
             <Header/>
             <Background/> 
+            <PopularDestinations/>
             <PricingPlans/>
             <ContactUs/>
             <Footer/>
