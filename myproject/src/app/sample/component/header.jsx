a78a3824b98505326afd657548fa1108847345e1
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2024-12-03 17:55:00 +0600

    changed the class to className in Header

--- myproject/src/app/sample/component/header.jsx
+++ myproject/src/app/sample/component/header.jsx
@@ -1,15 +1,15 @@
-export function Header({heading}){
+export function Header(){
     return (
         <>
-            <div class="h-16 bg-blue-500 w-full flex justify-between items-center p-4 fixed top-0 z-10">
-                <div class="text-xl md:text-3xl text-white font-bold pl-2 md:pl-5">
+            <div className="h-16 bg-blue-500 w-full flex justify-between items-center p-4 fixed top-0 z-10">
+                <div className="text-xl md:text-3xl text-white font-bold pl-2 md:pl-5">
                     Travel Explorer
                 </div>
-                <nav class="flex space-x-3 md:space-x-5 pr-2 md:pr-3">
-                    <a href="#" class="text-white text-lg md:text-xl hover:text-blue-200 hover:scale-105 transition-transform duration-300">Home</a>
-                    <a href="#" class="text-white text-lg md:text-xl hover:text-blue-200 hover:scale-105 transition-transform duration-300">Destinations</a>
-                    <a href="#" class="text-white text-lg md:text-xl hover:text-blue-200 hover:scale-105 transition-transform duration-300">Pricing</a>
-                    <a href="#" class="text-white text-lg md:text-xl hover:text-blue-200 hover:scale-105 transition-transform duration-300">Contact</a>
+                <nav className="flex space-x-3 md:space-x-5 pr-2 md:pr-3">
+                    <a href="#" className="text-white text-lg md:text-xl hover:text-blue-200 hover:scale-105 transition-transform duration-300">Home</a>
+                    <a href="#" className="text-white text-lg md:text-xl hover:text-blue-200 hover:scale-105 transition-transform duration-300">Destinations</a>
+                    <a href="#" className="text-white text-lg md:text-xl hover:text-blue-200 hover:scale-105 transition-transform duration-300">Pricing</a>
+                    <a href="#" className="text-white text-lg md:text-xl hover:text-blue-200 hover:scale-105 transition-transform duration-300">Contact</a>
                 </nav>
             </div>
         </>
